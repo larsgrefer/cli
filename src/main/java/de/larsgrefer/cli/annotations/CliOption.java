c83ecf6e9986d611f56fb6501ee4b65493e72ec0
Lars Grefer <larsgrefer@freenet.de>
2014-07-23 22:48:35 +0200

    + Added first annotation

---
+++ src/main/java/de/larsgrefer/cli/annotations/CliOption.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.larsgrefer.cli.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author lgrefer
+ */
+@Target(ElementType.FIELD)
+public @interface CliOption {
+	String name();
+	String longName();
+}
